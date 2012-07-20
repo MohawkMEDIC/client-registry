8fdb45afa282bb2645237a63973eb60dedc19b7b
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2012-07-20 19:23:40 +0000

    git-svn-id: https://fisheye.marc-hi.ca/svn/clientregistry/trunk@10 20676676-396a-054e-880a-9cdced9ab634

---
+++ MARC.HI.EHRS.CR.Messaging.Everest/MessageReceiver/UV/PixManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MARC.HI.EHRS.SVC.Messaging.Everest;
+
+namespace MARC.HI.EHRS.CR.Messaging.Everest.MessageReceiver.UV
+{
+    class PixManager : IEverestMessageReceiver
+    {
+        #region IEverestMessageReceiver Members
+
+        public MARC.Everest.Interfaces.IGraphable HandleMessageReceived(object sender, MARC.Everest.Connectors.UnsolicitedDataEventArgs e, MARC.Everest.Connectors.IReceiveResult receivedMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IUsesHostContext Members
+
+        /// <summary>
+        /// Gets or sets the context for the receiver
+        /// </summary>
+        public SVC.Core.HostContext Context
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region ICloneable Members
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ MARC.HI.EHRS.CR.Messaging.HAPI/PixPdqMessageHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MARC.HI.EHRS.SVC.Core.Services;
+
+namespace MARC.HI.EHRS.CR.Messaging.PixPdqv2
+{
+    /// <summary>
+    /// Message handler service
+    /// </summary>
+    public class PixPdqMessageHandler : IMessageHandlerService
+    {
+        #region IMessageHandlerService Members
+
+        /// <summary>
+        /// Start the v2 message handler
+        /// </summary>
+        public bool Start()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Stop the v2 message handler
+        /// </summary>
+        public bool Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IUsesHostContext Members
+
+        /// <summary>
+        /// Gets or sets the host context
+        /// </summary>
+        public SVC.Core.HostContext Context
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
