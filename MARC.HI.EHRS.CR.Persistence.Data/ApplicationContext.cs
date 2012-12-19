b0274085123f5f9966cb769f93aad3086bdda353
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2012-12-19 19:37:26 +0000

    Added license headers
    git-svn-id: https://fisheye.marc-hi.ca/svn/clientregistry/trunk@63 20676676-396a-054e-880a-9cdced9ab634

--- MARC.HI.EHRS.CR.Persistence.Data/ApplicationContext.cs
+++ MARC.HI.EHRS.CR.Persistence.Data/ApplicationContext.cs
@@ -1,5 +1,5 @@
-﻿/* 
- * Copyright 2008-2011 Mohawk College of Applied Arts and Technology
+﻿/**
+ * Copyright 2012-2012 Mohawk College of Applied Arts and Technology
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you 
  * may not use this file except in compliance with the License. You may 
@@ -12,11 +12,11 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
  * License for the specific language governing permissions and limitations under 
  * the License.
-
  * 
- * User: Justin Fyfe
- * Date: 08-24-2011
+ * User: fyfej
+ * Date: 19-7-2012
  */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
