db7face2c095f3d07f5dc8d0c5aa87723f99acdf
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2013-02-08 05:16:46 +0000

    Added missing files
    git-svn-id: https://fisheye.marc-hi.ca/svn/clientregistry/trunk@70 20676676-396a-054e-880a-9cdced9ab634

---
+++ MARC.HI.EHRS.CR.Presentation/ConsoleParameters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MohawkCollege.Util.Console.Parameters;
+using System.ComponentModel;
+using System.Collections.Specialized;
+
+namespace MARC.HI.EHRS.SVC.Presentation
+{
+    /// <summary>
+    /// Console parameters
+    /// </summary>
+    public class ConsoleParameters
+    {
+
+        /// <summary>
+        /// Show help
+        /// </summary>
+        [Parameter("?")]
+        [Parameter("help")]
+        public bool Help { get; set; }
+
+        /// <summary>
+        /// Console mode
+        /// </summary>
+        [Parameter("console")]
+        [Parameter("c")]
+        [Description("Run the Client Registry in interactive mode")]
+        public bool Interactive { get; set; }
+    }
+}
