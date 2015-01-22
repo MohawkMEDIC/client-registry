ad6acae6a1ae67ecfea7a06f83edd24696d367be
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2015-01-22 12:09:43 +0000

    Several CAT fixes.
    git-svn-id: https://fisheye.marc-hi.ca/svn/clientregistry/branches/1.0@195 20676676-396a-054e-880a-9cdced9ab634

---
+++ ClientRegistryAdmin/Controllers/PatientController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientRegistryAdmin.Models;
+using ClientRegistryAdmin.Util;
+
+namespace ClientRegistryAdmin.Controllers
+{
+    public class PatientController : Controller
+    {
+        //
+        // GET: /Patient/
+        public ActionResult Index()
+        {
+            return View(new PatientSearchModel());
+        }
+
+        /// <summary>
+        /// /Patient/Search
+        /// </summary>
+        [HttpPost]
+        public ActionResult Search(PatientSearchModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    model.IsError = true;
+                else
+                {
+                    model.Outcome = CrUtil.Search(model.FamilyName, model.GivenName, model.DateOfBirth);
+                    model.IsError = model.Outcome == null;
+                }
+            }
+            catch (Exception e)
+            {
+                model.IsError = true;
+                
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ ClientRegistryAdmin/Models/PatientMatch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientRegistryAdmin.Models
+{
+    /// <summary>
+    /// Patient record match
+    /// </summary>
+    public class PatientMatch
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public String GivenName { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public String FamilyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the dob
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the addr
+        /// </summary>
+        public String Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the gender
+        /// </summary>
+        public String Gender { get; set; }
+
+        /// <summary>
+        /// Gets the ECID
+        /// </summary>
+        public String Id { get; set; }
+
+        /// <summary>
+        /// Get the confidence
+        /// </summary>
+        public int Confidence { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mother's identifier
+        /// </summary>
+        public String MothersId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mother's name
+        /// </summary>
+        public String MothersName { get; set; }
+
+        /// <summary>
+        /// City
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// State
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Other identifiers
+        /// </summary>
+        public List<KeyValuePair<String, String>> OtherIds { get; set; }
+    }
+}
